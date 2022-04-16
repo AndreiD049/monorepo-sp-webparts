ca3ad40ccaf48dd65911729d3a563ad95859ffab
AndreiD049 <andrei.dimitrascu.94@gmail.com>
2022-04-16 12:41:50 +0300

    property-pane

--- packages/property-pane-access-control/src/AccessPropertyControl/IAccessControlProps.ts
+++
@@ -1,7 +0,0 @@
-import { IUserGroupPermissions } from './AccessControl';
-
-export interface IAccessControlProps {
-  permissions: string[];
-  selected: IUserGroupPermissions;
-  onChanged: (newValue: IUserGroupPermissions) => void;
-}
--- packages/property-pane-access-control/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { PropertyPaneAccessControl } from './AccessPropertyControl/PropertyPaneAccessControl';
-import { IUserGroupPermissions } from './AccessPropertyControl/AccessControl';
-import { canCurrentUser } from './dal/access-checking';
-
-export default PropertyPaneAccessControl;
-export { IUserGroupPermissions, canCurrentUser };
