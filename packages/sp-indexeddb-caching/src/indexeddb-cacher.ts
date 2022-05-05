c9ff413367ee8474b2d67ceaaf8fa8ee382384a7
AndreiD049 <andrei.dimitrascu.94@gmail.com>
2022-05-05 19:32:31 +0300

    sp-indexeddb-caching: started working on a poc

--- packages/sp-indexeddb-caching/src/indexeddb-cacher.ts
+++ packages/sp-indexeddb-caching/src/indexeddb-cacher.ts
@@ -1,3 +1,26 @@
-export function IndexedDBCacher() {
-    console.log('Hello cahcer');
+import { TimelinePipe } from '@pnp/core';
+import { Queryable } from '@pnp/queryable';
+import { ICacherConfig } from './cacher-config';
+
+export function IndexedDBCacher(config?: ICacherConfig) {
+    /**
+     * Test database health.
+     * Check if you can create a database, then if you can create a store
+     * if error: use fallback if provided, if now, throw an error
+     */
+    const CachingTimeline: TimelinePipe<any> = async (instance: Queryable) => {
+        instance.on.pre(async (url, init, result) => {
+            /**
+             * Check how it's done in the pnpjs package: https://github.com/pnp/pnpjs/blob/a9b15c4a8f517d99a14e626f2494bd8549435536/packages/queryable/behaviors/caching.ts
+             * Steps to be done here:
+             * check if url in cache
+             * if yes: check if cache not expired
+             */
+            return [url, init, result]
+        });
+        return instance;
+    };
+    return {
+        CachingTimeline,
+    }
 }
