56e52d20060217d6925637f44e325d2bc9fb4b76
Andrei Dimitrascu <Andrei.Dimitrascu@Katoennatie.com>
2023-04-03 17:08:42 +0300

    wrap Action log time logs

--- packages/sp-cip/src/webparts/cip/components/ActionLogTime/index.tsx
+++ packages/sp-cip/src/webparts/cip/components/ActionLogTime/index.tsx
@@ -1,45 +1,46 @@
-import { IconButton, Text } from 'office-ui-fabric-react';
-import * as React from 'react';
 import { ITaskOverview } from '@service/sp-cip/dist/models/ITaskOverview';
 import { IAction } from '@service/sp-cip/dist/services/action-service';
-import { formatHours } from '../../utils/hours-duration';
-import styles from './ActionLogTime.module.scss';
+import { IconButton, Text } from 'office-ui-fabric-react';
+import * as React from 'react';
 import { showDialog } from 'sp-components';
 import { DIALOG_ID_PANEL } from '../../utils/constants';
+import { GlobalContext } from '../../utils/GlobalContext';
+import { formatHours } from '../../utils/hours-duration';
 import { TimeLog } from '../TimeLog';
+import styles from './ActionLogTime.module.scss';
 
 export interface IActionLogTimeProps {
-    action: IAction;
-    task: ITaskOverview;
+  action: IAction;
+  task: ITaskOverview;
 }
 
 const iconButtonStyles = {
-    root: {
-        width: 20,
-        height: 20,
-    },
-    icon: {
-        fontSize: '.9em',
-    },
+  root: {
+    width: 20,
+    height: 20,
+  },
+  icon: {
+    fontSize: '.9em',
+  },
 };
 
 export const ActionLogTime: React.FC<IActionLogTimeProps> = (props) => {
     const [logged, setLogged] = React.useState(0);
     const [comment, setComment] = React.useState('');
 
-    React.useEffect(() => {
-        const indexOfPipe = props.action.Comment.indexOf('|');
-        setLogged(Number.parseFloat(props.action.Comment.substring(0, indexOfPipe)));
-        setComment(props.action.Comment.substring(indexOfPipe + 1))
-    }, []);
+  React.useEffect(() => {
+    const indexOfPipe = props.action.Comment.indexOf('|');
+    setLogged(Number.parseFloat(props.action.Comment.substring(0, indexOfPipe)));
+    setComment(props.action.Comment.substring(indexOfPipe + 1))
+  }, []);
 
-    const handleEdit = (): void => {
-        showDialog({
-                id: DIALOG_ID_PANEL,
-                dialogProps: { title: 'Log time', },
-                content: (<TimeLog task={props.task} dialogId={DIALOG_ID_PANEL} action={props.action} />)
-            })
-    };
+  const handleEdit = (): void => {
+    showDialog({
+      id: DIALOG_ID_PANEL,
+      dialogProps: { title: 'Log time', },
+      content: (<TimeLog task={props.task} dialogId={DIALOG_ID_PANEL} action={props.action} />)
+    })
+  };
 
     return (
         <div className={styles.container}>
